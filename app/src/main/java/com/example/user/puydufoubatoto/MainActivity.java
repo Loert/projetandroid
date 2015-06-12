a3fd93a3f438e26cbf8ad74705168abbedfc51be
Thomas Rollet <thomas.rollet@viacesi.fr>
2015-06-12 17:15:28 +0200

    Création de la première Activity

--- app/src/main/java/com/example/user/puydufoubatoto/MainActivity.java
+++ app/src/main/java/com/example/user/puydufoubatoto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.puydufoubatoto;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,7 +13,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Coucou
+        //Intent i = new Intent(MainActivity.this);
+        //startActivity(i);
     }
 
     @Override
